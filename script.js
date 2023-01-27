1ca0c8da762b57bf5584a85e8053089bcf0e5a3e
Tom Salter <tomjsalter@protonmail.com>
2023-01-27 14:22:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const gameBoard = (() => {
+  let board = [];
+  return { board };
+})();
+
+const player = (name) => {
+  return { name };
+};
+
+const displayController = (() => {
+    // logic to control the flow of the game
+})();
